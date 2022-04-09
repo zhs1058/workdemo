5da913c3d1849091b2e16a930e882400c168113e
zhs <1789888628@qq.com>
2022-04-09 10:49:18 +0800

    多线程

---
+++ src/thread/joinDemo/ConditionUseCase.java
@@ -0,0 +1,42 @@
+package thread.joinDemo;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ConditionUseCase {
+	
+	static Lock lock = new ReentrantLock();
+	static Condition condition = lock.newCondition();
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		conditionWait();
+		conditionSingle();
+	}
+
+	public static void conditionWait() {
+		lock.lock();
+		
+		try {
+			System.out.println("ËøµÈ´ý");
+			condition.await();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}finally {
+			lock.unlock();
+		}
+	}
+	
+	public static void conditionSingle() {
+		lock.lock();
+		try {
+			System.out.println("ËøÍ¨Öª");
+			condition.signal();
+		}finally {
+			lock.unlock();
+		}
+		
+	}
+}
---
+++ src/thread/joinDemo/WaitAndNotify.java
@@ -0,0 +1,63 @@
+package thread.joinDemo;
+
+import java.util.UUID;
+
+public class WaitAndNotify {
+
+	static boolean flag = true;
+	static Object lock = new Object();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Thread thread1 = new Thread(new Wait(),"线程1");
+		thread1.start();
+		
+		Thread thread2 = new Thread(new Single(),"线程2");
+		thread2.start();
+	}
+	
+	static class Wait implements Runnable{
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			synchronized(lock) {
+				while(flag) {
+					try {
+						System.out.println(Thread.currentThread().getName() + "正在被阻塞");
+						lock.wait();
+					} catch (InterruptedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+					System.out.println(Thread.currentThread().getName() + "正在执行");
+				}
+				
+			}
+		}
+		
+	}
+	
+	static class Single implements Runnable{
+
+		@Override
+		public void run() {
+			// TODO Auto-generated method stub
+			synchronized(lock) {
+				while(flag) {
+					flag = false;
+					lock.notify();
+					System.out.println(Thread.currentThread().getName() + "已经开始通知");
+				}
+			}
+			synchronized(lock) {
+				
+				System.out.println(Thread.currentThread().getName() + "正在被执行" + UUID.randomUUID());
+			}
+		}
+		
+	}
+
+}
+
+
