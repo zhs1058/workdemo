b685b3b652e136b9638be169db5896cb3e026289
zhs <1789888628@qq.com>
2024-08-04 16:21:17 +0800

    增加代码

--- src/test/test2.java
+++ src/test/test2.java
@@ -8,35 +8,6 @@ public class test2 {
 
 	public static void main(String[] args) {
 		
-		Scanner in = new Scanner(System.in);
-		int n = 0 , m = 0;
-		n = in.nextInt();
-		m = in.nextInt();
-		int[] dp = new int[m];
-		for(int i = 0; i < m; i ++) {
-			dp[i] = in.nextInt();
-		}
-		for(int v : dp) {
-			System.out.println(v);
-		}
-//		if(in.hasNext()) {
-//			n = in.nextInt();
-//			m = in.nextInt();
-//		}
-		System.out.println(n + m);
-		
-//		int a  = 7;
-//		int b = ++ a;
-//		int c = a++;
-//		System.out.println("a=" + a);
-//		System.out.println("b=" + b);
-//		System.out.println("c=" + c);
-//		AtomicLong al = new AtomicLong();
-//		LongAdder la = new LongAdder();
-//		System.out.println(9/2);
-//		int[] dp = new int[10];
-//		for(int s : dp) {
-//			System.out.println(s);
-//		}
+		System.out.println("我是世界上最可爱的代代");
 	}
 }
